d1047218c3bce7c9168a90c7582f2af7334e93d8
Сергей Душенбиев <agnium@mail.ru>
2022-02-28 11:28:52 +0300

    Java 6.1 rev 1

--- src/main/java/ru/netilogy/stats/StatsService.java
+++ src/main/java/ru/netilogy/stats/StatsService.java
@@ -5,68 +5,68 @@ public class StatsService {
     //Сумма всех продаж
 
     public double totalSalesSum(double[] salesPerMonth) {
-        double sum = 0;
-        for (double iArray : salesPerMonth) sum += iArray;
-        return sum;
+        double sumTotal = 0;
+        for (double monthlySales : salesPerMonth) {
+            sumTotal += monthlySales;
+        }
+        return sumTotal;
     }
 
     // Средняя сумма продаж в месяц
 
     public double averageMonthlySales(double[] salesPerMonth) {
-        double sum = 0;
-        for (double iArray : salesPerMonth) sum += iArray;
-        return sum / salesPerMonth.length;
+        double sumAverage = totalSalesSum(salesPerMonth);
+        return sumAverage / salesPerMonth.length;
     }
 
     // Номер месяца, в котором был пик продаж
 
     public double indexMonthOfMaxSales(double[] salesPerMonth) {
         double sumPerMonth = 0;
-        int month = 0;
+        int monthMax = 0;
         for (int i = 0; i < salesPerMonth.length; i++) {
             if (sumPerMonth <= salesPerMonth[i]) {
                 sumPerMonth = salesPerMonth[i];
-                month = i;
+                monthMax = i;
             }
         }
-        return month + 1;
+        return monthMax + 1;
     }
 
     // Номер месяца, в котором был минимум продаж
 
     public double indexMonthOfMinSales(double[] salesPerMonth) {
         double sumPerMonth = salesPerMonth[0];
-        int month = 0;
+        int monthMin = 0;
         for (int i = 0; i < salesPerMonth.length; i++) {
             if (salesPerMonth[i] <= sumPerMonth) {
                 sumPerMonth = salesPerMonth[i];
-                month = i;
+                monthMin = i;
             }
         }
-        return month + 1;
+        return monthMin + 1;
     }
 
     // Кол-во месяцев, в которых продажи были ниже среднего
 
     public double numberOfMonthBelowAverageSales(double[] salesPerMonth) {
-        double sum = 0;
-        int month = 0;
-        for (double iArray : salesPerMonth) sum += iArray;
-        double averageSum = sum / salesPerMonth.length;
-        for (double sumPerMonth : salesPerMonth) if (averageSum > sumPerMonth) month = month + 1;
-        return month;
+        int monthBelowAverageSales = 0;
+        double averageSum = averageMonthlySales(salesPerMonth);
+        for (double sumPerMonth : salesPerMonth) {
+            if (averageSum > sumPerMonth) monthBelowAverageSales = monthBelowAverageSales + 1;
+        }
+        return monthBelowAverageSales;
     }
 
 
     // Кол-во месяцев, в которых продажи были выше среднего
 
     public double numberOfMonthAboveAverageSales(double[] salesPerMonth) {
-        double sum = 0;
-        int month = 0;
-        for (double iArray : salesPerMonth) sum += iArray;
-        double averageSum = sum / salesPerMonth.length;
-        for (double sumPerMonth : salesPerMonth) if (averageSum < sumPerMonth) month = month + 1;
-        return month;
+        int monthAboveAverageSales = 0;
+        double averageSum = averageMonthlySales(salesPerMonth);
+        for (double sumPerMonth : salesPerMonth)
+            if (averageSum < sumPerMonth) monthAboveAverageSales = monthAboveAverageSales + 1;
+        return monthAboveAverageSales;
     }
 }
 
--- src/test/java/ru/netilogy/stats/StatsServiceTest.java
+++ src/test/java/ru/netilogy/stats/StatsServiceTest.java
@@ -1,5 +1,7 @@
 package ru.netilogy.stats;
+
 import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class StatsServiceTest {
